93038797e662b11c128e8ba1616a5e806d50c2b4
Artem Okhotnikov <art.okhotnikov@yandex.ru>
2023-08-02 14:46:12 +0400

    pages

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,23 @@ import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
   root: resolve(__dirname, "src"),
+  // base: "/dist/",
   build: {
     outDir: resolve(__dirname, "dist"),
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "src/index.html"),
+        login: resolve(__dirname, "src/login.html"),
+        registry: resolve(__dirname, "src/registry.html"),
+        notFound: resolve(__dirname, "src/404.html"),
+        error: resolve(__dirname, "src/500.html"),
+        messages: resolve(__dirname, "src/messages.html"),
+      },
+    },
   },
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, "src/partials"),
-      context: {
-        username: "John Doe",
-        title: "hello",
-      },
     }),
   ],
 });
